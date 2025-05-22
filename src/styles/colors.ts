91b4b30d15e9a4eccef5b1c22644810b25d25441
Keventon <kevertonguimaraes19@gmail.com>
2025-05-22 18:29:11 -0300

    adicionado fontfamily e cores

---
+++ src/styles/colors.ts
@@ -0,0 +1,14 @@
+export const colors = {
+  blue: "#364D9D",
+  blue_light: "#647AC7",
+  red_light: "#EE7979",
+  gray: {
+    1: "#1A181B",
+    2: "#3E3A40",
+    3: "#5F5B62",
+    4: "#9F9BA1",
+    5: "#D9D8DA",
+    6: "#EDECEE",
+    7: "#F7F7F8",
+  },
+};
