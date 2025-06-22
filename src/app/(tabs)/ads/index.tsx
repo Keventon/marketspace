45435ae84b4e499dc265608d0abb7ec41d28a6d7
Keventon <kevertonguimaraes19@gmail.com>
2025-06-22 16:12:56 -0300

    adicionado biblioteca de dropdown

--- src/app/(tabs)/ads/index.tsx
+++ src/app/(tabs)/ads/index.tsx
@@ -1,10 +1,22 @@
 import { CustomText } from "@/components/CustomText/indext";
 import { Header } from "@/components/Header";
 import { colors } from "@/styles/colors";
-import { ArrowLeft, CaretDown, Plus } from "phosphor-react-native";
+import { fontFamily } from "@/styles/fontFamily";
+import { Plus } from "phosphor-react-native";
+import { useState } from "react";
 import { StyleSheet, View } from "react-native";
+import { Dropdown } from "react-native-element-dropdown";
 
 export default function Ads() {
+  const [value, setValue] = useState(null);
+  const [isFocus, setIsFocus] = useState(false);
+
+  const data = [
+    { label: "Todos", value: "todos" },
+    { label: "Novo", value: "novo" },
+    { label: "Usado", value: "usado" },
+  ];
+
   return (
     <View style={styles.container}>
       <Header title="Meus anúncios" orientation="right" icon={<Plus />} />
@@ -14,25 +26,30 @@ export default function Ads() {
           <CustomText type="regular" fontSize={14} color={colors.gray[2]}>
             9 anúncios
           </CustomText>
-          <View
-            style={{
-              width: 112,
-              flexDirection: "row",
-              alignItems: "center",
-              padding: 8,
-              borderColor: colors.gray[5],
-              borderRadius: 6,
-              gap: 16,
-              borderWidth: 1,
+
+          <Dropdown
+            style={[
+              styles.dropdown,
+              isFocus && { borderColor: colors.blue_light },
+            ]}
+            placeholderStyle={styles.placeholderStyle}
+            selectedTextStyle={styles.selectedTextStyle}
+            inputSearchStyle={styles.inputSearchStyle}
+            iconStyle={styles.iconStyle}
+            data={data}
+            maxHeight={300}
+            labelField="label"
+            valueField="value"
+            placeholder={!isFocus ? "Todos" : "..."}
+            value={value}
+            onFocus={() => setIsFocus(true)}
+            onBlur={() => setIsFocus(false)}
+            onChange={(item) => {
+              setValue(item.value);
+              setIsFocus(false);
             }}
-          >
-            <View style={{ flex: 1 }}>
-              <CustomText type="regular" color={colors.gray[1]} fontSize={14}>
-                Todos
-              </CustomText>
-            </View>
-            <CaretDown color={colors.gray[3]} size={16} />
-          </View>
+            renderLeftIcon={() => null}
+          />
         </View>
       </View>
     </View>
@@ -52,5 +69,42 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "space-between",
+  },
+  dropdown: {
+    width: 120,
+    height: 50,
+    borderColor: colors.gray[5],
+    borderWidth: 0.5,
+    borderRadius: 6,
+    paddingHorizontal: 8,
+  },
+  icon: {
+    marginRight: 5,
+  },
+  label: {
+    position: "absolute",
+    backgroundColor: "white",
+    left: 22,
+    top: 8,
+    zIndex: 999,
+    paddingHorizontal: 8,
+    fontSize: 14,
+    fontFamily: fontFamily.regular,
+  },
+  placeholderStyle: {
+    fontSize: 14,
+    fontFamily: fontFamily.regular,
+  },
+  selectedTextStyle: {
+    fontSize: 16,
+    fontFamily: fontFamily.regular,
+  },
+  iconStyle: {
+    width: 20,
+    height: 20,
+  },
+  inputSearchStyle: {
+    height: 40,
+    fontSize: 14,
   },
 });
